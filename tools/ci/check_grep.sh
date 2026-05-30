fe8cb28a442953a8c46d195bd764b3aa591382dd
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2026-05-30 01:37:12 -0400

    Update check_grep.sh

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -19,14 +19,14 @@ if command -v rg >/dev/null 2>&1; then
 	if [ ! rg -P '' >/dev/null 2>&1 ] ; then
 		pcre2_support=0
 	fi
-	code_files="*/**/**.dm"
+	code_files="*/**/**.dm --glob '!DMCompiler_linux-x64/**'"
 	map_files="_maps/**/**.dmm"
 	shuttle_map_files="_maps/shuttles/**.dmm"
 	code_x_515="code/**/!(__byond_version_compat).dm"
 else
 	pcre2_support=0
 	grep=grep
-	code_files="-r --include=*/**/**.dm"
+	code_files="-r --exclude-dir=DMCompiler_linux-x64 --include=*/**/**.dm"
 	map_files="-r --include=_maps/**/**.dmm"
 	shuttle_map_files="-r --include=_maps/shuttles/**.dmm"
 	code_x_515="-r --include=code/**/!(__byond_version_compat).dm"
@@ -51,8 +51,8 @@ section "map issues"
 part "TGM"
 if $grep -U '^".+" = \(.+\)' $map_files;	then
 	echo
-    echo -e "${RED}ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!${NC}"
-    st=1
+	echo -e "${RED}ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!${NC}"
+	st=1
 fi;
 part "comments"
 if $grep '//' $map_files | $grep -v '//MAP CONVERTED BY dmm2tgm.py THIS HEADER COMMENT PREVENTS RECONVERSION, DO NOT REMOVE' | $grep -v 'name|desc'; then
@@ -63,8 +63,8 @@ fi;
 part "iconstate tags"
 if $grep '^\ttag = "icon' $map_files;	then
 	echo
-    echo -e "${RED}ERROR: Tag vars from icon state generation detected in maps, please remove them.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Tag vars from icon state generation detected in maps, please remove them.${NC}"
+	st=1
 fi;
 part "invalid map procs"
 if $grep '(new|newlist|icon|matrix|sound)\(.+\)' $map_files;	then
@@ -81,42 +81,42 @@ fi;
 part "common spelling mistakes"
 if $grep -i 'nanotransen' $map_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please remove the extra N(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please remove the extra N(s).${NC}"
+	st=1
 fi;
 if $grep 'NanoTrasen' $map_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please uncapitalize the T(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please uncapitalize the T(s).${NC}"
+	st=1
 fi;
 if $grep -i 'centcomm' $map_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in maps, please remove the extra M(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in maps, please remove the extra M(s).${NC}"
+	st=1
 fi;
 if $grep -i 'eciev' $map_files; then
 	echo
-    echo -e "${RED}ERROR: Common I-before-E typo detected in maps.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Common I-before-E typo detected in maps.${NC}"
+	st=1
 fi;
 if $grep -i 'maintainance|maintainence|maintenence' $map_files; then
-    echo
-    echo -e "${RED}ERROR: Misspelling(s) of 'maintenance' detected in maps, please fix.${NC}";
-    st=1
+	echo
+	echo -e "${RED}ERROR: Misspelling(s) of 'maintenance' detected in maps, please fix.${NC}";
+	st=1
 fi;
 
 section "whitespace issues"
 part "space indentation"
-if $grep '(^ {2})|(^ [^ * ])|(^    +)' $code_files; then
+if $grep '(^ {2})|(^ [^ * ])|(^	+)' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Space indentation detected, please use tab indentation.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Space indentation detected, please use tab indentation.${NC}"
+	st=1
 fi;
 part "mixed indentation"
 if $grep '^\t+ [^ *]' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Mixed <tab><space> indentation detected, please stick to tab indentation.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Mixed <tab><space> indentation detected, please stick to tab indentation.${NC}"
+	st=1
 fi;
 
 section "unit tests"
@@ -133,9 +133,9 @@ fi;
 section "516 Href Styles"
 part "byond href styles"
 if $grep "href[\s='\"\\\\]*\?" $code_files ; then
-    echo
-    echo -e "${RED}ERROR: BYOND requires internal href links to begin with \"byond://\".${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: BYOND requires internal href links to begin with \"byond://\".${NC}"
+	st=1
 fi;
 
 section "common mistakes"
@@ -225,95 +225,95 @@ fi;
 
 part "as anything on typeless loops"
 if $grep 'var/[^/]+ as anything' $code_files; then
-    echo
-    echo -e "${RED}ERROR: 'as anything' used in a typeless for loop. This doesn't do anything and should be removed.${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: 'as anything' used in a typeless for loop. This doesn't do anything and should be removed.${NC}"
+	st=1
 fi;
 
 part "as anything on internal functions"
 if $grep 'var\/(turf|mob|obj|atom\/movable).+ as anything in o?(view|range|hearers)\(' $code_files; then
-    echo
-    echo -e "${RED}ERROR: 'as anything' typed for loop over an internal function. These functions have some internal optimization that relies on the loop not having 'as anything' in it.${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: 'as anything' typed for loop over an internal function. These functions have some internal optimization that relies on the loop not having 'as anything' in it.${NC}"
+	st=1
 fi;
 
 part "common spelling mistakes"
 if $grep -i 'centcomm' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in code, please remove the extra M(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in code, please remove the extra M(s).${NC}"
+	st=1
 fi;
 if $grep -ni 'nanotransen' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please remove the extra N(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please remove the extra N(s).${NC}"
+	st=1
 fi;
 if $grep 'NanoTrasen' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please uncapitalize the T(s).${NC}"
-    st=1
+	echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please uncapitalize the T(s).${NC}"
+	st=1
 fi;
 if $grep -i 'eciev' $code_files; then
 	echo
-    echo -e "${RED}ERROR: Common I-before-E typo detected in code.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Common I-before-E typo detected in code.${NC}"
+	st=1
 fi;
 if $grep -i 'maintainance|maintainence|maintenence' $code_files; then
-    echo
-    echo -e "${RED}ERROR: Misspelling(s) of 'maintenance' detected in code, please fix.${NC}";
-    st=1
+	echo
+	echo -e "${RED}ERROR: Misspelling(s) of 'maintenance' detected in code, please fix.${NC}";
+	st=1
 fi;
 part "map json naming"
 if ls _maps/*.json | $grep "[A-Z]"; then
 	echo
-    echo -e "${RED}ERROR: Uppercase in a map .JSON file detected, these must be all lowercase.${NC}"
-    st=1
+	echo -e "${RED}ERROR: Uppercase in a map .JSON file detected, these must be all lowercase.${NC}"
+	st=1
 fi;
 
 part "Ineffective easing flags in animate()"
 if $grep 'easing\w*=\w*(EASE_IN|EASE_OUT|\(EASE_IN\w*\|\w*EASE_OUT\))' $code_files; then
-    echo
-    echo -e "${RED}ERROR: 'animate' was called with an easing argument and the default, LINEAR_EASING curve. This doesn't do anything and should be adjusted.${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: 'animate' was called with an easing argument and the default, LINEAR_EASING curve. This doesn't do anything and should be adjusted.${NC}"
+	st=1
 fi;
 
 part "map json sanity"
 for json in _maps/*.json
 do
-    map_path=$(jq -r '.map_path' $json)
-    while read map_file; do
-        filename="_maps/$map_path/$map_file"
-        if [ ! -f $filename ]
-        then
+	map_path=$(jq -r '.map_path' $json)
+	while read map_file; do
+		filename="_maps/$map_path/$map_file"
+		if [ ! -f $filename ]
+		then
 			echo
-            echo -e "${RED}ERROR: Found an invalid file reference to $filename in _maps/$json ${NC}"
-            st=1
-        fi
-    done < <(jq -r '[.map_file] | flatten | .[]' $json)
+			echo -e "${RED}ERROR: Found an invalid file reference to $filename in _maps/$json ${NC}"
+			st=1
+		fi
+	done < <(jq -r '[.map_file] | flatten | .[]' $json)
 done
 
 part "updatepaths validity"
 missing_txt_lines=$(find tools/UpdatePaths/Scripts -type f ! -name "*.txt" | wc -l)
 if [ $missing_txt_lines -gt 0 ]; then
-    echo
-    echo -e "${RED}ERROR: Found an UpdatePaths File that doesn't end in .txt! Please add the proper file extension!${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: Found an UpdatePaths File that doesn't end in .txt! Please add the proper file extension!${NC}"
+	st=1
 fi;
 
 number_prefix_lines=$(find tools/UpdatePaths/Scripts -type f | wc -l)
 valid_number_prefix_lines=$(find tools/UpdatePaths/Scripts -type f | $grep -P "\d+_(.+)" | wc -l)
 if [ $valid_number_prefix_lines -ne $number_prefix_lines ]; then
-    echo
-    echo -e "${RED}ERROR: Detected an UpdatePaths File that doesn't start with the PR number! Please add the proper number prefix!${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: Detected an UpdatePaths File that doesn't start with the PR number! Please add the proper number prefix!${NC}"
+	st=1
 fi;
 
 section "515 Proc Syntax"
 part "proc ref syntax"
 if $grep '\.proc/' $code_x_515 ; then
-    echo
-    echo -e "${RED}ERROR: Outdated proc reference use detected in code, please use proc reference helpers.${NC}"
-    st=1
+	echo
+	echo -e "${RED}ERROR: Outdated proc reference use detected in code, please use proc reference helpers.${NC}"
+	st=1
 fi;
 
 if [ "$pcre2_support" -eq 1 ]; then
@@ -372,13 +372,13 @@ else
 fi
 
 if [ $st = 0 ]; then
-    echo
-    echo -e "${GREEN}No errors found using $grep!${NC}"
+	echo
+	echo -e "${GREEN}No errors found using $grep!${NC}"
 fi;
 
 if [ $st = 1 ]; then
-    echo
-    echo -e "${RED}Errors found, please fix them and try again.${NC}"
+	echo
+	echo -e "${RED}Errors found, please fix them and try again.${NC}"
 fi;
 
 exit $st
